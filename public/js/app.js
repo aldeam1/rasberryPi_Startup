75caf2d00953c024e0f0044bd5594f1b74c84fda
Aldea <mihai.aldea@atos.net>
2019-01-07 17:19:36 +0200

    Updated FrontClient Bootstrap added

--- public/js/app.js
+++ public/js/app.js
@@ -3,18 +3,36 @@
 // functional programing
 //culoare buton in functie de stare
 
+// Old code pentru capture event la butoane--v-a fi sters mai tarziu
+//******************************************* */
+// $("#turn-on").click (function(){
+//     $.ajax({
+//         url:'/led/on',
+//         type: 'PUT'
+//     });
+//     // return alert('Button Turn On was pressed');
+// })
 
-$("#turn-on").click (function(){
-    $.ajax({
-        url:'/led/on',
-        type: 'PUT'
-    });
-    // return alert('Button Turn On was pressed');
-})
+// $("#turn-off").click (function(){
+//     $.ajax({
+//         url:'led/off',
+//         type: 'PUT'
+//     });
+// })
+//******************************************* */
 
-$("#turn-off").click (function(){
-    $.ajax({
-        url:'led/off',
-        type: 'PUT'
-    });
-})
+$("#led1").change(function(){
+    if (this.checked) {
+        $("#led1").style("content:'On'")
+        $.ajax({
+            url:'/led/on',
+            type: 'PUT'
+        });  
+    } else {
+        $("#led1").style("content:'Off'")
+        $.ajax({
+            url:'led/off',
+            type: 'PUT'
+        });
+    }
+});
